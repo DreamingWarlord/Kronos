f079101b1ee89efdaac12604a51ae2fcebee42de
DreamingWarlord <dreamingwarlord@tutanota.com>
2023-12-11 10:45:05 +0300

    Redesign

--- src/Main.c
+++
@@ -1,7 +0,0 @@
-#include "Common.h"
-
-
-int main()
-{
-	return 0;
-}
--- src/Util.c
+++
@@ -1,7 +0,0 @@
-#include "Common.h"
-
-void BugOn(char *expr_str, char *file, int line)
-{
-	printf("\x1B[1m%s:%d:\x1B[31;22m Assertion failed: \x1B[0m%s\n", file, line, expr_str);
-	exit(1);
-}
